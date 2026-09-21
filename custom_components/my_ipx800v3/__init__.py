52b01865a3c455e1797654aa33aa7a123e999401
amg0 <alexis.mermet@gmail.com>
2026-09-21 19:46:18 +0000

    fix: defaut port number when the geturl() returns a None value for the port number.

--- custom_components/my_ipx800v3/__init__.py
+++ custom_components/my_ipx800v3/__init__.py
@@ -254,8 +254,8 @@ async def async_setup_entry(
     base_url = get_url(hass, allow_external=False)
     parsed_url = urlparse(base_url)
     ip_address = parsed_url.hostname
-    port = parsed_url.port  # e.g., 8123
-
+    # port = parsed_url.port  # e.g., 8123
+    port = parsed_url.port or (443 if parsed_url.scheme == "https" else 80)
     # Configure the WebHook in the IPX 800
     if entry.data.get(CONF_AUTOMATIC_PUSH, True):
         await client.async_config_push(
